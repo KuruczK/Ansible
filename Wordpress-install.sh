333385a328ff30c6be191f1fab3e536e1fbaec93
KuruczK <33117677+KuruczK@users.noreply.github.com>
2018-02-16 10:21:55 +0100

    Update Wordpress-install.sh

--- Wordpress-install.sh
+++ Wordpress-install.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 sudo apt-get update
-#sudo apt-get upgrade -y
 sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible  -y
+sudo apt-get upgrade -y
 git clone https://github.com/andreipak/wordpress-ansible.git
 cd wordpress-ansible
 sudo ansible-playbook -i hosts playbook.yml
